68dde3c85e3894defe06fdd0af9047a1f1f3782f
nussam <nussam@nustechnology.com>
2017-04-30 21:25:18 +0700

    fix bug portfolio, header, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
-
-  get 'contacts/index'
-
-  get 'about_us/index'
-
-  get 'services/index'
-
   root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -15,4 +7,5 @@ Rails.application.routes.draw do
   resources :about_us
   resources :contacts
   resources :blogs
+  resources :home
 end
