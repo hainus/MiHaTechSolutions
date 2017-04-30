87e5513b3109ce06067c71daedff717d5c1bd35c
nussam <nussam@nustechnology.com>
2017-04-30 21:00:11 +0700

    refactor html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+
+  get 'contacts/index'
+
+  get 'about_us/index'
+
+  get 'services/index'
+
   root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :portfolios
+  resources :services
+  resources :about_us
+  resources :contacts
+  resources :blogs
 end
